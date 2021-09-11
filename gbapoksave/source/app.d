05483aee38250a3532ad963de4e03d73b7590c29
xdrie <xdrie@alt.icu>
2021-09-11 14:27:22 -0700

    print trainer name with lib

--- gbapoksave/source/app.d
+++ gbapoksave/source/app.d
@@ -1,9 +1,20 @@
 import std.stdio;
 import libspec;
+import std.file;
+import std.utf;
 
 void main(string[] args) {
 	writeln("gbapoksave");
-	
+
 	auto sav_path = args[1];
 	writefln("save path: %s", sav_path);
+	auto savfile_data = cast(ubyte[]) std.file.read(sav_path);
+
+	// try loading the save
+	auto loaded_save = gba_read_main_save(cast(const(ubyte)*) savfile_data);
+
+	auto trainer = gba_get_trainer(loaded_save);
+	ushort[16] trainer_name_buf;
+	gba_text_to_ucs2(cast(ushort*) trainer_name_buf, cast(ubyte*) trainer.name, 7);
+	writefln("trainer name: %s", cast(wchar[16]) trainer_name_buf);
 }
