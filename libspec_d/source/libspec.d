03bdf6a7c08aa09e90d7bfbaf38bef0212045eba
xdrie <xdrie@alt.icu>
2021-09-11 13:53:02 -0700

    stub d proj

---
+++ libspec_d/source/libspec.d
@@ -0,0 +1,12 @@
+extern (C):
+
+// #include "game_gb.h"
+
+// #include "game_nds.h"
+// #include "game_ndsi.h"
+
+/**
+ * @mainpage LibSPEC is a pokemon save editing library written in C.
+ */
+
+//__LIBSPEC_H__
