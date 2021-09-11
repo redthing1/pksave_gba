92fbc712a907784e61900404eb0c922e94478444
xdrie <xdrie@alt.icu>
2021-09-11 13:50:31 -0700

    dont include removed games

--- libspec/include/libspec.h
+++ libspec/include/libspec.h
@@ -5,10 +5,10 @@
 #include "pkm.h"
 #include "stat.h"
 #include "checksum.h"
-#include "game_gb.h"
+// #include "game_gb.h"
 #include "game_gba.h"
-#include "game_nds.h"
-#include "game_ndsi.h"
+// #include "game_nds.h"
+// #include "game_ndsi.h"
 
 /**
  * @mainpage LibSPEC is a pokemon save editing library written in C.
