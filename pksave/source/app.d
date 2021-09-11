bd0f913b086424f7ae2ff439d39fd5055a9cd665
xdrie <xdrie@alt.icu>
2021-09-11 14:52:10 -0700

    print trainer info

--- pksave/source/app.d
+++ pksave/source/app.d
@@ -5,15 +5,20 @@ import std.file;
 import util;
 
 void main(string[] args) {
-	writeln("PKSAVE");
+	writeln("> PKSAVE");
 
 	auto sav_path = args[1];
-	writefln("save path: %s", sav_path);
+	// writefln("save path: %s", sav_path);
 	auto savfile_data = cast(ubyte[]) std.file.read(sav_path);
 
 	// try loading the save
 	auto loaded_save = gba_read_main_save(cast(const(ubyte)*) savfile_data);
 
-	auto trainer = gba_get_trainer(loaded_save);	
-	writefln("trainer name: %s", decode_gba_text(trainer.name));
+	auto trainer = gba_get_trainer(loaded_save);
+	writeln("TRAINER");
+	writefln("  NAME: %s", decode_gba_text(trainer.name));
+	writefln("  GENDER: %s", trainer.gender == 0 ? "M" : "F");
+	writefln("  PLAYTIME: %s", gba_time_to_string(trainer.time_played));
+	writeln("ITEMS");
+	writefln("  MONEY: %s", gba_get_money(loaded_save));
 }
--- pksave/source/util.d
+++ pksave/source/util.d
@@ -1,6 +1,7 @@
 module util;
 
 import std.utf;
+import std.string;
 import libspec;
 
 wchar[] decode_gba_text(ubyte[] gba_text) {
@@ -10,4 +11,8 @@ wchar[] decode_gba_text(ubyte[] gba_text) {
     gba_text_to_ucs2(cast(ushort*) decoded_buf, cast(ubyte*) gba_text, text_len);
 
     return cast(wchar[]) decoded_buf;
+}
+
+string gba_time_to_string(gba_time_t time) {
+    return format("%03d:%02d", time.hours, time.minutes);
 }
