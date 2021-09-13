bce0df7ded2e6b4464ae47a2e85bdf7bdd4e0c16
xdrie <xdrie@alt.icu>
2021-09-12 20:40:00 -0700

    match sgs rom

--- pksave/source/pokegame.d
+++ pksave/source/pokegame.d
@@ -105,11 +105,16 @@ enum PkmnROMSpeciesDataOffsets : uint {
     OFFSET_BULBASAUR_SGS_138 = 0xA6BCEC,
 }
 
-enum ubyte[] MASTERBALL_ITEM_DATA = [
+enum ubyte[] MASTERBALL_ITEM_DATA_FR = [
         0xC7, 0xBB, 0xCD, 0xCE, 0xBF, 0xCC, 0x00, 0xBC, 0xBB, 0xC6, 0xC6, 0xFF,
         0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0xCC, 0x4E, 0x3D,
         0x08, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00,
         0x00, 0x1D, 0x1E, 0x0A, 0x08, 0x00, 0x00, 0x00, 0x00
+    ];
+
+enum ubyte[] MASTERBALL_ITEM_DATA_MATCH = [
+        0xFF, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0xCC, 0x4E, 0x3D,
+        0x08, 0x00, 0x00, 0x03, 0x00
     ];
 
 enum int ITEMTBL_ENTRY_LENGTH = 28;
@@ -117,12 +122,17 @@ enum int ITEMTBL_ENTRY_LENGTH = 28;
 enum PkmnROMItemTblOffsets : uint {
     /*
         masterball (ID: 0x01) offset
-        data looks like:
+        in FRLG, data looks like:
         C7 BB CD CE BF CC 00 BC BB C6 C6 FF 00 00 01 00 00 00 00 00 CC 4E 3D 08 00 00 03 00 00 00 00 00 02 00 00 00 1D 1E 0A 08 00 00 00 00
+        in SGS, data looks like:
+        C7 D5 E7 E8 D9 E6 00 BC D5 E0 E0 FF 00 00 01 00 00 00 00 00 CC 4E 3D 08 00 00 03 00 68 05 46 08 02 00 00 00 1D 1E 0A 08 00 00 00 00
+
+        a good place to match: (+11)
+        FF 00 00 01 00 00 00 00 00 CC 4E 3D 08 00 00 03 00
     */
     OFFSET_MASTERBALL_FR_U = 0x3DB054, // fire red en-us
     // OFFSET_MASTERBALL_LG_U = 0x3DAE64, // leaf green en-us THIS IS WRONG
-    OFFSET_MASTERBALL_SGS_138 = 0xA6BCEC,
+    OFFSET_MASTERBALL_SGS_138 = 0x3DB054,
 }
 
 enum PkmnROMDetect {
@@ -243,6 +253,17 @@ class PkmnROM {
         }
     }
 
+    uint get_itemtbl_offset_for_rom(PkmnROMDetect rom_type) {
+        switch (rom_type) {
+        case PkmnROMDetect.FIRE_RED_U:
+            return PkmnROMItemTblOffsets.OFFSET_MASTERBALL_FR_U;
+        case PkmnROMDetect.SGS_138:
+            return PkmnROMItemTblOffsets.OFFSET_MASTERBALL_SGS_138;
+        default:
+            assert(0, "Unknown");
+        }
+    }
+
     bool detect_rom_type() {
         // check third byte of species data
         if (rom_buf[PkmnROMSpeciesDataOffsets.OFFSET_BULBASAUR_FR_U + 2] == 0x31) {
@@ -284,20 +305,19 @@ class PkmnROM {
         }
 
         // ensure that masterball data is found at offset
-        // read 44 bytes from ROM at address
-        auto itemtbl_offset = get_itemtbl_offset_for_rom(rom_type);
+        auto itemtbl_offset = get_itemtbl_offset_for_rom(rom_type) + 11;
         // auto offset = get_itemtbl_offset_for_rom(PkmnROMDetect.SGS_138);
 
-        auto itemtbl_offset_end = itemtbl_offset + itemtbl_ENTRY_LENGTH;
-        auto itemtbl_rom_slice = rom_buf[itemtbl_offset .. itemtbl_offset_end];
+        auto itemtbl_offset_end = itemtbl_offset + MASTERBALL_ITEM_DATA_MATCH.length;
+        auto itemtbl_rom_slice = rom_buf[itemtbl_offset.. itemtbl_offset_end];
         // writefln("rom slice (0x%06x-0x%06x): %s", offset, offset_end, rom_slice);
 
         // compare with masterball seq
-        auto is_itemtbl_equal = equal(itemtbl_rom_slice, BULBASAUR_SPECIES_DATA);
+        auto is_itemtbl_equal = equal(itemtbl_rom_slice, MASTERBALL_ITEM_DATA_MATCH);
 
         if (!is_itemtbl_equal) {
-            assert(0, format("rom (detected %s) specdata slice (0x%06x-0x%06x) did not match BULBASAUR seq: %s",
-                    rom_type, itemtbl_offset, itemtbl_offset_end, itemtbl_rom_slice));
+            assert(0, format("rom (detected %s) itemtbl slice (0x%06x-0x%06x) did not match MASTERBALL match seq: %s (target: %s)",
+                    rom_type, itemtbl_offset, itemtbl_offset_end, itemtbl_rom_slice, MASTERBALL_ITEM_DATA_MATCH));
         }
 
         return is_specdata_equal && is_itemtbl_equal;
