b996f96878834fcbdc76bf027149cbcd1970a80b
redthing1 <redthing1@alt.icu>
2022-04-16 20:52:12 -0700

    check save binary

--- libspec_d/test/test_gba_save.d
+++ libspec_d/test/test_gba_save.d
@@ -4,6 +4,8 @@ import std.stdio;
 import std.format;
 import std.array;
 import std.file;
+import core.stdc.stdio;
+import core.stdc.string;
 
 import libspec;
 
@@ -19,11 +21,18 @@ unittest {
     // read pc to struct
     auto pc_struct = gba_unpack_pc_data(loaded_save_orig);
     // writefln("pc: %s", pc_struct);
-    writefln("size of pc struct: %s", pc_struct.sizeof);
+    // writefln("size of pc struct: %s", pc_struct.sizeof);
+
+    // // do something evil
+    // auto raw_fake_pc = gba_get_pc(loaded_save_dup);
+    // memset(raw_fake_pc, 0xff, gba_pc_t.sizeof);
 
     // pack struct to pc
-    gba_pack_pc_data(loaded_save_dup, &pc_struct);
+    // gba_pack_pc_data(loaded_save_dup, &pc_struct);
 
     // ensure that they're the same
-    assert(*loaded_save_orig.data == *loaded_save_dup.data, "save file data is not equal");
+    auto loaded_save_orig_bin = loaded_save_orig.data[0..GBA_UNPACKED_SIZE];
+    auto loaded_save_dup_bin = loaded_save_dup.data[0..GBA_UNPACKED_SIZE];
+    // assert(*loaded_save_orig.data == *loaded_save_dup.data, "save file data is not equal");
+    assert(loaded_save_orig_bin == loaded_save_dup_bin, "save file data is not equal");
 }
